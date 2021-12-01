03030e862d267d45ef7ff99b12a74f58290cf192
Faver Izquierdo <jhonfa199418@gmail.com>
2021-12-01 04:00:02 -0500

    nuevo

--- src/app/components/pages/panel-admin/panel-admin.component.ts
+++ src/app/components/pages/panel-admin/panel-admin.component.ts
@@ -1,37 +1,35 @@
-import { Component, OnInit } from "@angular/core";
-import { EstudianteService } from "src/app/Servicio/ServiceEstudiante/service-estudiante.service"; 
-import { TipoDocumentoService } from "src/app/Servicio/ServiceTipoDocumento/service-tipo-documento.service"; 
-import { Estudiante } from "src/app/modelo2/persona";
+import { Component, OnInit } from '@angular/core';
+import { EstudianteService } from 'src/app/Servicio/ServiceEstudiante/service-estudiante.service';
+import { TipoDocumentoService } from 'src/app/Servicio/ServiceTipoDocumento/service-tipo-documento.service';
+import { Estudiante } from 'src/app/modelo2/persona';
 
 @Component({
-  selector: "app-panel-admin",
-  templateUrl: "./panel-admin.component.html",
-  styleUrls: ["./panel-admin.component.css"],
+  selector: 'app-panel-admin',
+  templateUrl: './panel-admin.component.html',
+  styleUrls: ['./panel-admin.component.css'],
 })
 export class PanelAdminComponent implements OnInit {
   tipoDocumentos: any = [];
 
-  // estudiante: Estudiante = {
-  //   idEstudiante: "",
-  //   grado: "",
-  //   director: "",
-  //   nombreAcudiente: "",
-  //   unaPersona: {
-  //     unTipoDocumento: {
-  //       idTipoDocumento : 1,
-  //     },
-  //     numeroDocumento: "",
-  //     nombre: "",
-  //     apellido: "",
-  //     fechaNacimiento: "",
-  //     telefono: "",
-  //     direccion: "",
-  //     barrio: "",
-  //     fechaCreacion: "",
-  //     fechaModificacion: "",
-  //     estado?: string;
-
-  //   }
+  estudiante: Estudiante = {
+    grado: '',
+    director: '',
+    nombreAcudiente: '',
+    unaPersona: {
+      unTipoDocumento: {
+        idTipoDocumento: 1,
+        nombre: '',
+      },
+      numeroDocumento: '',
+      nombre: '',
+      apellido: '',
+      fechaNacimiento: '',
+      telefono: '',
+      direccion: '',
+      barrio: '',
+      fechaCreacion: '',
+    },
+  };
 
   // };
 
@@ -47,30 +45,34 @@ export class PanelAdminComponent implements OnInit {
   // telefono: "",
   // director: "",
   // barrio: "",
-
+  fecha: string;
   constructor(
     private tipoDocumentoService: TipoDocumentoService,
     private estudianteService: EstudianteService
-  ) {}
+  ) {
+    var f = new Date();
+    var _fecha = f.getDate() + '/' + (f.getMonth() + 1) + '/' + f.getFullYear();
+    this.fecha = _fecha;
+  }
 
   ngAfterViewInit() {}
 
   ngOnInit(): void {
     this.getTipoDocumento();
   }
-  estudiante: Estudiante = new Estudiante();
 
   save(estudiante: Estudiante) {
-    alert(estudiante)
-    console.log(estudiante)
-    // this.estudianteService.saveEstudiantes(estudiante).subscribe(
-    //   (res) => {
-    //     console.log(res);
-    //   },
-    //   (err) => {
-    //     console.log(err);
-    //   }
-    // );
+    this.estudiante.unaPersona.fechaCreacion = this.fecha;
+    alert(estudiante);
+    console.log(estudiante);
+    this.estudianteService.saveEstudiantes(estudiante).subscribe(
+      (res) => {
+        console.log(res);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   getTipoDocumento() {
--- src/app/modelo2/persona.ts
+++ src/app/modelo2/persona.ts
@@ -1,10 +1,8 @@
 export class Estudiante {
-  idEstudiante: string;
   grado: string;
   director: string;
   nombreAcudiente: string;
   unaPersona: {
-    idPersona: number;
     unTipoDocumento: {
       idTipoDocumento: number;
       nombre: string;
@@ -17,7 +15,5 @@ export class Estudiante {
     direccion: string;
     barrio: string;
     fechaCreacion: string;
-    fechaModificacion?: string;
-    estado: string;
   };
 }
