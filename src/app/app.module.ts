53935ee6015242fd0cc282a333532132bc05c149
Faver Izquierdo <jhonfa199418@gmail.com>
2021-12-01 12:25:22 -0500

    faver

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/header/header/header.component";
-import { loginComponent } from "./components/pages/login/login.component";
-import { AdminForumComponent } from "./components/pages/admin-forum/admin-forum.component";
-import { StudentForumComponent } from "./components/pages/student-forum/student-forum.component";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { PanelAdminComponent } from "./components/pages/panel-admin/panel-admin.component";
-import { NavBarAdminComponent } from "./components/nav-bar-admin/nav-bar-admin.component";
-import { HeaderAdminComponent } from "./components/header-admin/header-admin.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { CdkAccordionModule } from "@angular/cdk/accordion";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatIconModule } from "@angular/material/icon";
-import { GenerarReporteComponent } from "./components/pages/generar-reporte/generar-reporte.component";
-import { AdminHomeComponent } from "./components/pages/admin-home/admin-home.component";
-import { StudentValidateComponent } from "./components/pages/student-validate/student-validate.component";
-import { StudentBenefitComponent } from "./components/pages/student-benefit/student-benefit.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header/header.component';
+import { loginComponent } from './components/pages/login/login.component';
+import { AdminForumComponent } from './components/pages/admin-forum/admin-forum.component';
+import { StudentForumComponent } from './components/pages/student-forum/student-forum.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PanelAdminComponent } from './components/pages/panel-admin/panel-admin.component';
+import { NavBarAdminComponent } from './components/nav-bar-admin/nav-bar-admin.component';
+import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatIconModule } from '@angular/material/icon';
+import { GenerarReporteComponent } from './components/pages/generar-reporte/generar-reporte.component';
+import { AdminHomeComponent } from './components/pages/admin-home/admin-home.component';
+import { StudentValidateComponent } from './components/pages/student-validate/student-validate.component';
+import { StudentBenefitComponent } from './components/pages/student-benefit/student-benefit.component';
 import { HttpClientModule } from '@angular/common/http';
+import { TablaPersonaComponent } from './components/tabla-persona/tabla-persona.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { HttpClientModule } from '@angular/common/http';
     AdminHomeComponent,
     StudentValidateComponent,
     StudentBenefitComponent,
+    TablaPersonaComponent,
   ],
   imports: [
     BrowserModule,
@@ -45,7 +47,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatSliderModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
